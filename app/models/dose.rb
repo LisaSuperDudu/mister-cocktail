312884b8cdc37cff7791c8e2d6dadba9373674c0
Lisa Durand <lisa@superdudu.net>
2015-10-29 15:55:24 +0100

    finishing model validations + creation cocktail controller + resource cocktail

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,17 @@
 class Dose < ActiveRecord::Base
-  belongs_to :coktail
+  validates :description, presence: true, uniqueness: true
+
+  belongs_to :cocktail
   belongs_to :ingredient
+
+  validates :cocktail_id, presence: true
+  validates :ingredient_id, presence: true
+
+  # validates_uniqueness_of :cocktail_id, :scope => [:ingredient_id]
+  # validates_uniqueness_of :ingredient_id, :scope => [:cocktail_id]
+
+  # validates :cocktail_id, :uniqueness => { :scope => [:cocktail_id, :ingredient_id] }
+
+  # validates_uniqueness_of :description, scope: [:cocktail_id, :ingredient_id]
+
 end
